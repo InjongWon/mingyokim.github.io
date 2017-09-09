8d6cf9557103d093a12a0ae57ab2fb1a19e5c617
Min Gyo <mingyo.kim3@gmail.com>
2017-09-08 20:09:26 -0700

    init commit

---
+++ js/script.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+  $("#thumb-aupera").css("height", $("#box-aupera").height() + 2);
+});
+
+$(document).ready(function() {
+  $("#thumb-snapcat").css("height", $("#box-snapcat").height() + 2);
+});
+
+$(document).ready(function() {
+  $("#thumb-busnapper").css("height", $("#box-busnapper").height() + 2);
+});
+
+$(document).ready(function() {
+  $("#thumb-airbnb").css("height", $("#box-airbnb").height() + 2);
+});
+
+$(window).resize(function() {
+  $("#thumb-aupera").css("height", $("#box-aupera").height() + 2);
+});
+
+$(window).resize(function() {
+  $("#thumb-snapcat").css("height", $("#box-snapcat").height() + 2);
+});
+
+$(window).resize(function() {
+  $("#thumb-airbnb").css("height", $("#box-airbnb").height() + 2);
+});
